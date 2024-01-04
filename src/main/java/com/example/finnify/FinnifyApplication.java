594311c0885a0fc67b55c3d77c47863bd8f932b3
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-01-05 01:06:05 +0200

    init

---
+++ src/main/java/com/example/finnify/FinnifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.finnify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinnifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinnifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finnify/FinnifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finnify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinnifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
